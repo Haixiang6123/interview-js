1ff973045271c606ecb38e72d0c8d67729916e20
Haixiang <haixiang6123@gmail.com>
2019-09-08 15:24:05 -0700

    New debounce function

--- debounce&throttle/debounce.js
+++ debounce&throttle/debounce.js
@@ -2,19 +2,34 @@
  * The function won't be called until stop moving in interval ms
  * @param fn
  * @param interval
+ * @param immediate
  * @returns {Function}
  */
-function debounce(fn, interval) {
+function debounce(fn, interval, immediate) {
     let timeout
     return function () {
         const context = this
         const args = arguments
 
-        clearTimeout(timeout)
+        if (timeout) {
+            clearTimeout(timeout)
+        }
 
-        timeout = setTimeout(function () {
-            // `this` points to current element
-            fn.apply(context, arguments)
-        }, interval)
+        if (immediate) {
+            const callNow = !timeout
+            timeout = setTimeout(function () {
+                timeout = null
+            }, interval)
+
+            if (callNow) {
+                fn.apply(context, args)
+            }
+        }
+        else {
+            timeout = setTimeout(function () {
+                // `this` points to current element
+                fn.apply(context, args)
+            }, interval)
+        }
     }
 }
